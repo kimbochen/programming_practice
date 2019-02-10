9b0910b0fc6f6bd139816d2c5272922d468229ab
K-mach <chentenghung@gmail.com>
2019-02-10 23:23:38 +0800

    Algorithm for Y, M, P computation finished

--- probA.cpp
+++ probA.cpp
@@ -2,11 +2,13 @@
 #include <sstream>
 #include <string>
 
-#define MIN(a,b) (stoi(a) < stoi(b)) ? a : b
+#define MIN(a,b) (a < b) ? a : b
 
 using namespace std;
 
 string probA(void);
+
+inline bool isEven(char);
 
 int main(void)
 {
@@ -17,9 +19,7 @@ int main(void)
 
     for (int i = 0; i < T; i++)
     {
-        ss << "Case #" << i+1 << ':';
-        
-        ss << probA() << '\n';
+        ss << "Case #" << i+1 << ':' << probA();
     }
 
     cout << ss.str();
@@ -33,14 +33,67 @@ string probA(void)
 
     cin >> N;
 
+    int M, P, Q;
+    int n = N.size();
+
     string X = N;
-    int i = 0, n = X.size();
+    
+    for (int i = 0; i < n; i++)
+    {
+        if (!isEven(X[i]))
+        {
+            X[i] --;
 
-    while (i < n && (X[i]-'0') % 2 == 0) i++;
+            for (int j = i+1; j < n; j++) X[j] = '8';
 
-    X[i++] --;
+            break;
+        }
+    }
 
-    while (i < n) X[i++] = '8';
+    string Y = N;
 
-    return X;
+    for (int i = 0; i < n; i++)
+    {
+        if (!isEven(Y[i]))
+        {
+            if (Y[i] == '9')
+            {
+                Y[i] = '0';
+
+                for (int j = i-1; j >= 0; j--)
+                {
+                    if (Y[j] != '8')
+                    {
+                        Y[j] += 2;
+                        break;
+                    }
+                    else Y[j] = '0';
+                }
+
+                if (Y[0] == '0')
+                {
+                    Y = "2" + Y;
+                    n ++;
+                }
+            }
+            else Y[i] ++;
+
+            for (int j = i+1; j < n; j++) Y[j] = '0';
+
+            break;
+        }
+    }
+
+    Q = stoi(N);
+
+    M = Q - stoi(X);
+
+    P = stoi(Y) - Q;
+
+    return to_string(MIN(M,P)) + "\n";
+}
+
+bool isEven(char c)
+{
+    return ((c-'0') % 2 == 0);
 }
