e2f17d4407f9191c27db06131dd134d853ca1594
K-mach <chentenghung@gmail.com>
2019-02-15 00:17:51 +0800

    B: Deleted class Fraction and changed container to vector

--- roundA/probB.cpp
+++ roundA/probB.cpp
@@ -1,98 +1,32 @@
 #include <algorithm>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
-class Fraction
-{
-public:
-    Fraction(long nom_, long den_): nom(nom_), den(den_)
-    {
-        reduce();
-    }
-
-    Fraction operator+(const Fraction &obj)
-    {
-        long fac = gcd(den, obj.den);
-
-        long nom_ = nom * (obj.den / fac) + obj.nom * (den / fac);
-
-        long den_ = (den / fac) * obj.den;
-
-        return Fraction(nom_, den_);
-    }
-
-    Fraction operator*(const Fraction &obj)
-    {
-        long lr_fac = gcd(nom, obj.den);
-        long rl_fac = gcd(den, obj.nom);
-
-        long nom_ = (nom / lr_fac)*(obj.nom / rl_fac);
-        long den_ = (den / rl_fac)*(obj.den/lr_fac);
-
-        return Fraction(nom_, den_);
-    }
-
-    double toDouble(void)
-    {
-        return ((double) nom / den);
-    }
-
-private:
-    long nom, den;
-
-    void reduce(void)
-    {
-        long factor = gcd(nom, den);
-
-        nom /= factor;
-        den /= factor;
-    }
-
-    long gcd(long a, long b)
-    {
-        return (a % b == 0) ? b : gcd(b, a%b);
-    }
-};
+using ull = unsigned long long;
 
 double probB(void);
 
-double suffixSum(int[],int,int);
+double suffixSum(vector<int>&,int,int);
 
-int searchMin(int[],int,int,double);
+int searchMin(vector<int>&,int,int,double);
 
 int main(void)
 {
-    long n, d;
+    int T;
+    stringstream ss;
 
-    cin >> n >> d;
+    cin >> T;
 
-    Fraction a(n,d);
+    for (int i = 0; i < T; i++)
+    {
+        ss << "Case #" << i+1 << ": ";
+        ss << fixed << probB() << '\n';
+    }
 
-    cin >> n >> d;
-
-    Fraction b(n,d);
-
-    Fraction c = a + b;
-
-    Fraction e = a * b;
-
-    cout << "c: " << c.toDouble() << '\n';
-
-    cout << "e: " << e.toDouble() << '\n';
-    //int T;
-    //stringstream ss;
-
-    //cin >> T;
-
-    //for (int i = 0; i < T; i++)
-    //{
-    //    ss << "Case #" << i+1 << ": ";
-    //    ss << fixed << probB() << '\n';
-    //}
-
-    //cout << ss.str();
+    cout << ss.str();
 
     return 0;
 }
@@ -100,18 +34,16 @@ int main(void)
 double probB(void)
 {
     int N, K;
-    int *V = nullptr;
-    double EX = 0.0;
 
     cin >> N >> K;
 
-    V = new int[N];
+    vector<int> V(N);
 
     for (int i = 0; i < N; i++) cin >> V[i];
 
-    sort(V, V+N);
+    sort(V.begin(), V.end());
 
-    EX = suffixSum(V, N, 0) / N;
+    double EX = suffixSum(V, N, 0) / N;
 
     if (K > 0)
     {
@@ -126,7 +58,7 @@ double probB(void)
     return EX;
 }
 
-double suffixSum(int V[], int N, int s)
+double suffixSum(vector<int> &V, int N, int s)
 {
     double sum = 0.0;
 
@@ -135,7 +67,7 @@ double suffixSum(int V[], int N, int s)
     return sum;
 }
 
-int searchMin(int V[], int lf, int rt, double EX)
+int searchMin(vector<int> &V, int lf, int rt, double EX)
 {
     if (lf < rt)
     {
